c444a1443cdd9cf117fae0f51142da0a761e1903
Gabriel Sales <gabrielsales07112018@gmail.com>
2024-07-03 22:57:34 -0300

    Criando o primeiro componente

---
+++ src/components/Main.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+import './Main.css'
+
+export default class Main extends Component {
+    state = {
+        newTask: '',
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            newTask: e.target.value,
+        })
+    }
+
+    render() {
+        const {newTask} = this.state
+
+        return (
+            <div className='main'>
+                <h1>{newTask}</h1>
+
+                <form action='#'>
+                    <input onChange={this.handleChange} type='text' />
+                    <button type='submit'>Enviar</button>
+                </form>
+            </div>
+        )
+    }
+}
